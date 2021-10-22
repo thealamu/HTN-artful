860335d78e04956a1a7c16cd8e6d46dc1a270a4f
Faithfulness Alamu <vaguemail369@gmail.com>
2021-10-22 21:03:46 +0100

    revert: "feat: make root handler testable"
    This reverts commit 9b084e1df8ea3f939bbe7fa8890bed9778a8cc35.

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	if err := start(); err != nil {
-		log.Println(err)
-	}
-}
-
-func start() error {
-	log.Println("Starting HTTP Server on", getRunAddr())
+	fmt.Println("Starting HTTP Server on", getRunAddr())
 	mux := http.DefaultServeMux
 	mux.HandleFunc("", rootHandler)
-	return http.ListenAndServe(getRunAddr(), mux)
+	http.ListenAndServe(getRunAddr(), mux)
 }
 
 func getRunAddr() string {
