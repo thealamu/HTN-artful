9b084e1df8ea3f939bbe7fa8890bed9778a8cc35
Faithfulness Alamu <vaguemail369@gmail.com>
2021-10-22 21:01:09 +0100

    feat: make root handler testable

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Starting HTTP Server on", getRunAddr())
+	if err := start(); err != nil {
+		log.Println(err)
+	}
+}
+
+func start() error {
+	log.Println("Starting HTTP Server on", getRunAddr())
 	mux := http.DefaultServeMux
 	mux.HandleFunc("", rootHandler)
-	http.ListenAndServe(getRunAddr(), mux)
+	return http.ListenAndServe(getRunAddr(), mux)
 }
 
 func getRunAddr() string {
