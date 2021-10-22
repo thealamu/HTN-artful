d50d198522fe14505551441582cda08f16b86b4a
Faithfulness Alamu <vaguemail369@gmail.com>
2021-10-22 20:50:45 +0100

    feat: init root handler

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	addr := getRunAddr()
+	fmt.Println("Starting HTTP Server on", addr)
+	http.ListenAndServe(addr, http.HandlerFunc(rootHandler))
+}
+
+func getRunAddr() string {
+	return ":8000"
 }
